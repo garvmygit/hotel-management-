6b6fcfec3d83363de28187706ad848133d987355
GARV GUPTA <160923425+garvmygit@users.noreply.github.com>
2025-03-05 09:54:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,158 +1,3 @@
-// const http = require("http");
-// const fs = require("fs");
-// const path = require("path");
-
-
-// const usersFilePath = path.join(__dirname, "users.json");
-
-
-// const readUsersFromFile = () => {
-//     if (!fs.existsSync(usersFilePath)) {
-//         return {};
-//     }
-//     const data = fs.readFileSync(usersFilePath, "utf8");
-//     return JSON.parse(data || "{}");
-// };
-
-
-// const saveUsersToFile = (users) => {
-//     fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2), "utf8");
-// };
-
-
-// const serveStatic = (res, filePath, contentType) => {
-//     fs.readFile(filePath, (err, content) => {
-//         if (err) {
-//             if (err.code === "ENOENT") {
-//                 res.writeHead(404, { "Content-Type": "text/plain" });
-//                 res.end("404 Not Found");
-//             } else {
-//                 res.writeHead(500, { "Content-Type": "text/plain" });
-//                 res.end("500 Internal Server Error");
-//             }
-//         } else {
-//             res.writeHead(200, { "Content-Type": contentType });
-//             res.end(content);
-//         }
-//     });
-// };
-
-
-// const handleRequest = (req, res) => {
-//     const url = req.url;
-
-//     if (url.startsWith("/public/")) {
-//         const filePath = `.${url}`;
-//         const extname = path.extname(filePath);
-//         let contentType = "text/plain";
-
-//         switch (extname) {
-//             case ".css":
-//                 contentType = "text/css";
-//                 break;
-//             case ".js":
-//                 contentType = "application/javascript";
-//                 break;
-//             case ".jpg":
-//             case ".jpeg":
-//                 contentType = "image/jpeg";
-//                 break;
-//             case ".png":
-//                 contentType = "image/png";
-//                 break;
-//             case ".gif":
-//                 contentType = "image/gif";
-//                 break;
-//             case ".svg":
-//                 contentType = "image/svg+xml";
-//                 break;
-//             default:
-//                 contentType = "application/octet-stream";
-//         }
-
-//         serveStatic(res, filePath, contentType);
-//     } else if (url === "/" || url === "/index.html") {
-//         serveStatic(res, "./views/index.html", "text/html");
-//     } else if (url === "/register" && req.method === "GET") {
-//         serveStatic(res, "./views/register.html", "text/html");
-//     }
-//     else if (url === "/portfolio" && req.method === "GET") {
-//         serveStatic(res, "./views/portfolio.html", "text/html");
-//     } 
-//     else if (url === "/department" && req.method === "GET") {
-//         serveStatic(res, "./views/department.html", "text/html");
-//     }
-//     else if (url === "/contact" && req.method === "GET") {
-//         serveStatic(res, "./views/contact.html", "text/html");
-//     }
-//     else if (url === "/about" && req.method === "GET") {
-//         serveStatic(res, "./views/about.html", "text/html");
-//     }
-//     else if (url === "/subdepartment" && req.method === "GET") {
-//         serveStatic(res, "./views/subdepartment.html", "text/html");
-//     }else if (url === "/register" && req.method === "POST") {
-//         let body = "";
-//         req.on("data", (chunk) => {
-//             body += chunk;
-//         });
-//         req.on("end", () => {
-//             const parsedBody = new URLSearchParams(body);
-//             const username = parsedBody.get("username");
-//             const password = parsedBody.get("password");
-
-//             if (username && password) {
-//                 const users = readUsersFromFile();
-//                 if (users[username]) {
-//                     // User already exists
-//                     res.writeHead(302, { Location: "/register?error=exists" });
-//                 } else {
-//                     // Save new user
-//                     users[username] = password;
-//                     saveUsersToFile(users);
-//                     res.writeHead(302, { Location: "/login" });
-//                 }
-//                 res.end();
-//             } else {
-//                 res.writeHead(302, { Location: "/register?error=invalid" });
-//                 res.end();
-//             }
-//         });
-//     } else if (url === "/login" && req.method === "GET") {
-//         serveStatic(res, "./views/login.html", "text/html");
-//     } else if (url === "/login" && req.method === "POST") {
-//         let body = "";
-//         req.on("data", (chunk) => {
-//             body += chunk;
-//         });
-//         req.on("end", () => {
-//             const parsedBody = new URLSearchParams(body);
-//             const username = parsedBody.get("username");
-//             const password = parsedBody.get("password");
-
-//             const users = readUsersFromFile();
-//             if (users[username] && users[username] === password) {
-//                 res.writeHead(302, { Location: "/dashboard" });
-//             } else {
-//                 res.writeHead(302, { Location: "/login?error=invalid" });
-//             }
-//             res.end();
-//         });
-//     } else if (url === "/dashboard") {
-//         serveStatic(res, "./views/dashboard.html", "text/html");
-//     } else {
-//         res.writeHead(404, { "Content-Type": "text/plain" });
-//         res.end("404 Not Found");
-//     }
-// };
-
-// // Start server
-// const server = http.createServer(handleRequest);
-// server.listen(8080, () => {
-//     console.log("Server running at http://localhost:8080/");
-// });
-
-
-
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
@@ -177,15 +22,14 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 
 // Serve views (HTML files)
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "index.html")));
-app.get("/about", (req, res) => res.sendFile(path.join(__dirname, "views", "about.html")));
 app.get("/contact", (req, res) => res.sendFile(path.join(__dirname, "views", "contact.html")));
 app.get("/portfolio", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "portfolio.html"));
 });
-const pages = ["register","login", "subdepartment","department" ,"thank-you"];
+const pages = ["register","login", "subdepartment","department" ,"thank-you","rooms","home"];
 pages.forEach((page) => {
-    app.get(`/${page}`, (req, res) => {
-        res.sendFile(path.join(__dirname, "views", `${page}.html`));
+    app.get(/${page}, (req, res) => {
+        res.sendFile(path.join(__dirname, "views", ${page}.html));
     });
 });
 
@@ -232,4 +76,4 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`));
+app.listen(PORT, () => console.log(Server running at http://localhost:${PORT}/));
